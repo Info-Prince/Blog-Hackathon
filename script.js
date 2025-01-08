e23471078f174e53f3976cb5ecb6c3dafb2bbf62
Prince Kumar <princeinfo053@gmail.com>
2025-01-08 09:52:53 +0000

    Sidebar event added. and smooth transition added

---
+++ script.js
@@ -0,0 +1,31 @@
+const toggleBtn = document.getElementById('toggle-btn');
+toggleBtn.addEventListener('click', (event) => {
+    
+    const sidebar = document.getElementById('sidebar');
+    
+    if(sidebar.hasAttribute('class', 'sidebar-active')) {
+        //hide sidebar
+        sidebar.style.position = 'absolute';        
+        sidebar.style.visibility = 'hidden';
+        sidebar.style.opacity = '0';
+        sidebar.style.translate = '-100%';
+
+        sidebar.removeAttribute('class', 'sidebar-active');
+
+        // rotate icon
+        document.getElementById('toggle-active-img').style.rotate = '-90deg';
+    }
+    else {
+        sidebar.style.position = 'relative';
+        sidebar.style.visibility = 'visible';
+        sidebar.style.opacity = '1';
+        sidebar.style.translate = '0';
+
+        sidebar.setAttribute('class', 'flex container sidebar-active');
+
+        // rotate icon to normal
+        document.getElementById('toggle-active-img').style.rotate = '90deg';
+
+    }
+    
+})
